a7e01413a64151c3562b61ac2217ead86ab7596c
slipperstree <slipperstree@gmail.com>
2018-09-05 02:42:55 +0900

    设置最大最小取值范围后可以拖动滚动条来设置数字

--- max7219.ts
+++ max7219.ts
@@ -8,6 +8,7 @@ namespace max7219led64 {
      * Turn on a led at specific position. 
      */
     //% blockId=“turnOnLed” block="turn on Led at x:%x|y:%y"
+    //% x.min=0 x.max=4 y.min=0 y.max=4
     export function turnOnLed(x:number, y:number) {
         led.plot(x, y)
     }
@@ -16,6 +17,7 @@ namespace max7219led64 {
      * Turn off a led at specific position. 
      */
     //% blockId=“turnOffLed” block="turn off Led at x:%x|y:%y"
+    //% x.min=0 x.max=4 y.min=0 y.max=4
     export function turnOffLed(x:number, y:number) {
         led.unplot(x, y)
     }
