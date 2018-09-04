fbfee2bd6565c796544dccb32e7fb862ad14f99d
slipperstree <slipperstree@gmail.com>
2018-09-05 01:49:03 +0900

    可以正常显示积木方块的初级版本

---
+++ max7219.ts
@@ -0,0 +1,14 @@
+/**
+ * Pick some fruit and peel it.
+ */
+//% weight=100 icon="\uf1db" color=#EC7505
+namespace pmbmax7219 {
+
+    /**
+     * xxxxxxx
+     */
+    //% blockId=xxx block="turn on Led at x:%x|y:%y"
+    export function turnOnLed(x:number, y:number) {
+
+    }
+}
