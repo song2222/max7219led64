5cc59460706baeadbaf5a8a1d31896ace296df88
slipperstree <slipperstree@gmail.com>
2018-09-05 23:59:39 +0900

    增加了全屏闪烁的积木。

--- max7219.ts
+++ max7219.ts
@@ -1,8 +1,42 @@
 /**
+ * Types of speed level
+ */
+enum FlashSpeed {
+
+    //% block=fast
+    fast = 100,
+
+    //% block=normal
+    normal = 300,
+
+    //% block=slow
+    slow = 500
+}
+
+/**
  * Pick some fruit and peel it.
  */
 //% weight=100 icon="\uf1db" color=#EC7505
 namespace max7219led64 {
+
+    let maxX = 4;
+    let maxY = 4;
+
+    function turnOnAllLeds() {
+        for (let x = 0; x <= maxX; x++) {
+            for (let y = 0; y <= maxY; y++) {
+                led.plot(x, y);
+            }
+        }
+    }
+
+    function turnOffAllLeds() {
+        for (let x = 0; x <= maxX; x++) {
+            for (let y = 0; y <= maxY; y++) {
+                led.unplot(x, y);
+            }
+        }
+    }
 
     /**
      * Turn on a led at specific position. 
@@ -10,7 +44,7 @@ namespace max7219led64 {
     //% blockId=“turnOnLed” block="turn on Led at x:%x|y:%y"
     //% x.min=0 x.max=4 y.min=0 y.max=4
     export function turnOnLed(x:number, y:number) {
-        led.plot(x, y)
+        led.plot(x, y);
     }
 
     /**
@@ -19,6 +53,20 @@ namespace max7219led64 {
     //% blockId=“turnOffLed” block="turn off Led at x:%x|y:%y"
     //% x.min=0 x.max=4 y.min=0 y.max=4
     export function turnOffLed(x:number, y:number) {
-        led.unplot(x, y)
+        led.unplot(x, y);
+    }
+
+    /**
+     * Flash all leds some times.
+     */
+    //% blockId=flashAllLed block="flash all leds times %times | with speed %speed"
+    export function flashAllLed(times:number, speed:FlashSpeed) {
+
+        for (let index = 0; index < times; index++) {
+            turnOnAllLeds();
+            basic.pause(speed);
+            turnOffAllLeds();
+            basic.pause(speed);
+        }
     }
 }
