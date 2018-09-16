e6be3cdee29d5f741ab439e8df5c3bf4bb0fc723
slipperstree <slipperstree@gmail.com>
2018-09-16 14:55:34 +0900

    为了解决microbit内存不足导致无法使用完整点阵字库的问题， 增加一个从外部储存芯片24CXX读取字库的功能。（目前支持24C256和24C128两种I2C协议的EEPROM） 包装了microbit内建I2C函数。 但是内建的I2C函数的接口过于简单，导致目前无法以单字节写入，只能以双字节写入。（提供的接口没办法适配24C256的写时序） 读取没有问题，既可以单字节也可以单字节。

---
+++ i2c24C256.ts
@@ -0,0 +1,55 @@
+namespace max7219led64 {
+
+    /**
+     * Write a 16-bit data into 24CXX.
+     * Note that data address must be a 16-bit data for 24C128 or 24C256.
+     * If you use a low capacity eeprom chip like 24C02, the data address should be a 8-bit data. So you need change 'NumberForamt.Int16BE' to 'NumberForamt.UInt8BE'.
+     * @param i2cAddr 7-bit address of 24CXX chip, not inclued the 8th bit write/read flag
+     * @param dataAddr address of data you want to write at
+     * @param data data value
+    */
+    export function write24C256_Int16(i2cAddr:number, dataAddr:number, data:number)  {
+        pins.i2cWriteNumber(
+            i2cAddr,
+            dataAddr * 65536 + data,
+            NumberFormat.Int32BE,
+            false
+            )
+    }
+
+    /**
+     * Read a 8-bit data from 24CXX.
+     * Note that data address must be a 16-bit data for 24C128 or 24C256.
+     * If you use a low capacity eeprom chip like 24C02, the data address should be a 8-bit data. So you need change 'NumberForamt.Int16BE' to 'NumberForamt.UInt8BE'.
+     * @param i2cAddr  7-bit address of 24CXX chip, not inclued the 8th bit write/read flag
+     * @param dataAddr address of data you want to read at
+    */
+    export function read24C256_Int8(i2cAddr:number, dataAddr:number) : number  {
+        // Set data address we want to 'read' by a 'dummy write'.
+        pins.i2cWriteNumber(
+            i2cAddr,
+            dataAddr,
+            NumberFormat.UInt16BE,
+            true
+            )
+            return pins.i2cReadNumber(80, NumberFormat.UInt8BE, false);
+    }
+
+    /**
+     * Read a 16-bit data from 24CXX.
+     * Note that data address must be a 16-bit data for 24C128 or 24C256.
+     * If you use a low capacity eeprom chip like 24C02, the data address should be a 8-bit data. So you need change 'NumberForamt.Int16BE' to 'NumberForamt.UInt8BE'.
+     * @param i2cAddr  7-bit address of 24CXX chip, not inclued the 8th bit write/read flag
+     * @param dataAddr address of data you want to read at
+    */
+    export function read24C256_Int16(i2cAddr:number, dataAddr:number) : number  {
+        // Set data address we want to 'read' by a 'dummy write'.
+        pins.i2cWriteNumber(
+            80,
+            dataAddr,
+            NumberFormat.UInt16BE,
+            true
+            )
+            return pins.i2cReadNumber(80, NumberFormat.UInt16BE, false);
+    }
+}
